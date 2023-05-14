236fc931f30edd1246b7e9f9165765be48569b0a
Sergey Skobelev <gforgx@protonmail.com>
2023-05-14 21:39:41 +0300

    Several changes

--- scripts/sloc.sh
+++ scripts/sloc.sh
@@ -1,7 +1,7 @@
 wc -l configure.ac Makefile.in common.h util.* slice.* map.* json.* \
-	source.* entry.* \
-	dir.* mozilla.* keep.* \
-	configuration.* ipc.* server.* \
-	ui/listbox.* ui/editor.* ui/window.c cmd.* \
+	entry.* \
+	providers/* \
+	ipc.* \
+	ui/*.c ui/*.h \
 	perl.* search.* main.c | awk -F ' ' '{s+=$1} END {print s}'
 
